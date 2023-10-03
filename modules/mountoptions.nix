807e4b62be0f144fbe8693e4304f5eb692bec4cb
iamcult <101368650+iamcult@users.noreply.github.com>
2023-10-03 20:55:22 +0000

    fix: update mountoptions.nix

--- modules/mountoptions.nix
+++ modules/mountoptions.nix
@@ -6,7 +6,6 @@
     "/home".options = [ "subvol=home" "compress=zstd" ];
     "/nix".options = [ "subvol=nix" "compress=zstd" "noatime" ];
     "/persist".options = [ "subvol=persist" "compress=zstd" ];
-    "/etc/nixos".options = [ "subvol=nixosconfig" "compress=zstd" ];
     "/var/log".options = [ "subvol=log" "compress=zstd" ];
   };
 }
